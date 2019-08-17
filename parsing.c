ccc31fc89294047eeb1bd604a11851b9eb391d55
jorgezafra94 <947@holbertonschool.com>
2019-08-17 18:03:15 +0000

    [Update]: Buffer size fixed and calloc function

--- parsing.c
+++ parsing.c
@@ -1,45 +1,68 @@
-#include "holberton.h"
-
-#define DELIM " \\`'#*&\n"
-
+#include "shell.h"
+/**
+ * Counter - counts the number of lim found in the input
+ * @L: the input string;
+ * @lim: character to find inside the L string
+ * Return: number of characters found
+ */
+int Counter(char *L, char *lim)
+{
+	int ct = 0, num = 0;
+	if (lim != NULL)
+	{
+		while (L && L[ct] != '\0')
+		{
+			if (L[ct] != lim[0])
+			{
+				if (L[ct + 1] == lim[0] || L[ct + 1] == '\0')
+					num++;
+			}
+			ct++;
+		}
+	}
+	return (num);
+}
+/**
+ * parsing - create an array of pointers depending of the delimit characters
+ * @line: input of the user
+ * Return: an array of pointers of n size
+ */
 char **parsing(char *line)
 {
-	char *str = NULL;
-	char *str2 = NULL, *token = NULL, **p = NULL;
-	int length = 0, j = 0, i = 0, buffsize = 10;
+	char *token = NULL, **p = NULL, *str;
+	int length = 0, j = 0, i = 0, buffsize = 0;
 
-	str = line;
-	p = malloc((buffsize) * sizeof(char *));
-	if (p == NULL)
+	if (line == NULL)
+		return (NULL);
+	buffsize = Counter(line, " ");
+	p = _calloc((buffsize + 1), sizeof(char *));
+	if (!p)
 	{
 		perror("No memory");
 		exit(EXIT_FAILURE);
 	}
-	p[buffsize] = NULL;
-        token = strtok(str, DELIM);
-	j = 0;
-	while (token != NULL)
+/*store the token partition inside **p */
+        token = strtok(line, " \n");
+	while (token)
 	{
 		while (token[length] != '\0')
 			length++;
-		p[j] = malloc((length + 1) * sizeof(char));
+		p[j] = _calloc((length + 1), sizeof(char));
 		if (p[j] == NULL)
 		{
-			while (j >= 0)
-			{
-				free(p[j]);
-				--j;
-			}
-			free(p);
+			free_grid(p, j);
 			perror("No memory");
 			exit(EXIT_FAILURE);
 		}
+/*fill the pointer with the content of token*/
 		for (i = 0; i < length; i++)
 			p[j][i] = token[i];
 		p[j][i] == '\0';
 		length = 0;
 		j++;
-		token = strtok(NULL, DELIM);
+/*get the next element*/
+		token = strtok(NULL, " \n");
 	}
+	p[j] = NULL;
         return (p);
 }

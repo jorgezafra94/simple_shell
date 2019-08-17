e9b7ca5ee9b0b77278c6ffb3dea0c4a52a646465
diegolopezq95 <927@holbertonschool.com>
2019-08-17 22:10:29 +0000

    [Update] add new functions

--- shell.h
+++ shell.h
@@ -9,11 +9,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 char *_getline(void);
 char **parsing(char *line);
 char *_getline_av(void);
 void free_grid(char **grid, int height);
 void _forky(char **p, char *line, int a);
 void *_calloc(unsigned int nmemb, unsigned int size);
+char **checkbin(char **b);
+void *_realloc2(char *ptr, unsigned int old_size, unsigned int new_size);
+
 #endif

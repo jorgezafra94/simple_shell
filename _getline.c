10025c78c188eb4716dd09c9326da7648b55a062
jorgezafra94 <947@holbertonschool.com>
2019-08-17 18:05:15 +0000

    [Update]: Fixed unitialized variable error

--- _getline.c
+++ _getline.c
@@ -1,24 +1,34 @@
-#include "holberton.h"
+#include "shell.h"
 /**
  * _getline - function to read what the user writes
+ * Return: line in sucess otherwise NULL.
  */
 char  *_getline(void)
 {
 	char *line = NULL;
-	ssize_t bufsize = 0, num = 1, pos = 0;
+	ssize_t bufsize = 0, num = 0, pos = 0;
 
+/* this step is for not get an uninitialized variable in valgrind*/
+	write(STDOUT_FILENO, "$ ", 2);
+	num = getline(&line, &bufsize, stdin);
+/*infinity loop while is different of EOF or error*/
 	while (num != -1)
 	{
-		printf("$ ");
-		num = getline(&line, &bufsize, stdin);
+		pos = 0;
 		if (!(line[0] == '\n'))
 		{
 			while (line[pos] != '\0')
 			{
 				if (line[pos] == '\n')
-					return line;
+					return (line);
 				pos++;
 			}
 		}
+		write(STDOUT_FILENO, "$ ", 2);
+		num = getline(&line, &bufsize, stdin);
 	}
-}	
+/* to get an end of line when the shell finish*/
+	write(STDOUT_FILENO, "\n", 1);
+	free(line);
+	return (NULL);
+}

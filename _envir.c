844c74ac27b02c83a9eeef7d35afe98c88045e50
diegolopezq95 <927@holbertonschool.com>
2019-08-19 13:37:10 +0000

    [Update]: it works!

--- _envir.c
+++ _envir.c
@@ -1,6 +1,22 @@
 #include "shell.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
  * _isenv - finds if line input is env
  * @p: input of user
  */
@@ -30,7 +46,7 @@ int _isenv(char *p)
 }
 /**
  * _env - function to print the environment
- * @environ:.
+ * @environ: points to an array of pointers to strings called the "environment"
  */
 void _env(void)
 {
@@ -38,7 +54,7 @@ void _env(void)
 
         for (i = 0; environ[i] != NULL; i++)
         {
-                write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+                write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
                 write(STDOUT_FILENO, "\n", 1);
         }
 }

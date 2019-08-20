f71565a514a0e4ff19c8c03e6ace673f571923ee
jorgezafra94 <947@holbertonschool.com>
2019-08-20 18:48:59 +0000

    [Feature] function that goes through the environmental variables and get the HOME variable

---
+++ _gethome.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * _gethome - funtion to get env HOME
+ * @environ: points to an array of pointers to strings called the "environment"
+ */
+char *_gethome(void)
+{
+	int i, j, k, cont = 0;
+	char str[] = "HOME=";
+	char *home;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '\0'; j++)
+		{
+			if (cont == 5)
+				break;
+			if (environ[i][j] == str[j])
+				cont++;
+			else
+				break;
+		}
+		if (cont == 5)
+			break;
+	}
+	home = environ[i];
+	for (k = 0; k < 5; k++)
+	{
+		home++;
+	}
+	return (home);
+}

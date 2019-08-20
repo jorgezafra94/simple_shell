d74781605c96eba2f4f51f7e83455b00196d48ac
jorgezafra94 <947@holbertonschool.com>
2019-08-20 21:26:34 +0000

    [Update] adding new functions in order to read the path

--- checkbin.c
+++ checkbin.c
@@ -1,45 +1,91 @@
 #include "shell.h"
+/**
+ *
+ *
+ *
+ */
+char *_getpath(void)
+{
+	int i, j, k = 0, cont = 0;
+	char str[] = "PATH=";
+	char *path;
 
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '\0'; j++)
+		{
+			if (cont == 5)
+				break;
+			if (environ[i][j] == str[j])
+				cont++;
+			else
+				break;
+		}
+		if (cont == 5)
+			break;
+	}
+	while (environ[i][k] != '\0')
+		k++;
+	path = _calloc(k + 1, sizeof(char));
+	k = 5;
+	while (environ[i][k] != '\0')
+	{
+		path[k - 5] = environ[i][k];
+		k++;
+	}
+	return (path);
+}
 /**
  * checkbin - checks if arg[0] has /bin/
- * @b: 
+ * @b: array of pointers
  * Return: 0.
  */
 
 char **checkbin(char **b)
 {
-	unsigned int i, j, flag = 0;
-	char bin[6] = "/bin/";
+	unsigned int i = 0, j = 0, k = 0;
+	struct stat veri;
+	char *path, *tokens, *buf;
+	char *valor;
 
 	i = _strlen(b[0]);
 	if (b == NULL || i == 0)
 		return (NULL);
-	j = 0;
-	while (j < 5)
+
+	if (stat(b[0], &veri) == 0)
+		return (b);
+	else
 	{
-		flag = 0;
-		if (b[0][j] != '\0')
+		path = _getpath();
+		tokens = strtok(path, ":");
+		while (tokens != NULL)
 		{
-			if (bin[j] == b[0][j])
-			{
+			while (tokens[j] != '\0')
 				j++;
-				if (j == 5)
-				{
-					flag = 1;
-					break;
-				}
+			buf = _calloc((j + 2), sizeof(char));
+			if (buf == NULL)
+			{
+				perror("No memory");
+				exit(EXIT_FAILURE);
 			}
-		        else 
-				break;
+			for (k = 0; k < j; k++)
+				buf[k] = tokens[k];
+			buf[k] = '/';
+			valor = str_concat(buf, b[0]);
+			if (stat(valor, &veri) == 0)
+			{
+				b[0] = _realloc2(buf, b[0], i, _strlen(valor));
+				free(path);
+				free(buf);
+				free(valor);
+				return (b);
+			}
+			tokens = strtok(NULL, ":");
+			j = 0;
+			free(buf);
+			free(valor);
 		}
-		else
-		{
-			break;
-		}
+		free(path);
+		return (b);
 	}
-	if (flag == 0)
-	{
-		b[0] = _realloc2(b[0], i, (i + 5));
-	}
-	return (b);
 }

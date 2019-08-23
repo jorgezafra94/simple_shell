316e29498a4cdee681f6d0ea329f9279a0ab3d75
diegolopezq95 <927@holbertonschool.com>
2019-08-23 18:37:57 +0000

    [Feature]: adding more errors

--- _writerr.c
+++ _writerr.c
@@ -48,44 +48,34 @@ void _put_err(char **p, int loop)
  */
 void _builtinerr(char **p)
 {
-	char str1[3] = "cd";
-	char str2[5] = "exit";
-	int i = 0, j = 0, cont = 0;
+	char str1[3] = "cd", str2[5] = "exit", str3[5] = "help";
+	int i = 0, j = 0, cont = 0, cont2 = 0;
 
 	while (p[0][j] != '\0')
-	{
 		j++;
-	}
 	if (j == 2)
 	{
-		while (i < 2)
-		{
+		for (; i < 2; i++)
 			if (p[0][i] == str1[i])
 				cont++;
-			i++;
-		}
 		if (cont == 2)
-		{
-			write(STDERR_FILENO, p[0], 2);
-			write(STDERR_FILENO, ": can't cd to ", 14);
-			write(STDERR_FILENO, p[1], _strlen(p[1]));
-			write(STDERR_FILENO, "\n", 1);
-		}
+			_errorcd(p);
 	}
 	if (j == 4)
 	{
-		while (i < 4)
-		{
+		for (i = 0 ; i < 4; i++)
 			if (p[0][i] == str2[i])
 				cont++;
-			i++;
-		}
 		if (cont == 4)
-		{
-			write(STDERR_FILENO, p[0], 4);
-			write(STDERR_FILENO, ": Illegal number: ", 18);
-			write(STDERR_FILENO, p[1], _strlen(p[1]));
-			write(STDERR_FILENO, "\n", 1);
-		}
+			_errorexit(p);
+	}
+	if (j == 4)
+        {
+                for (i = 0; i < 4; i++)
+			if (p[0][i] == str3[i])
+				cont2++;
+		if (cont2 == 4)
+			_errorhelp(p);
 	}
 }
+	

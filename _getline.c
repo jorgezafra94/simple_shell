089e63e3bdba5135955839c8b9970d6a12ed1aed
jorgezafra94 <947@holbertonschool.com>
2019-08-25 17:40:21 +0000

    [Fix] handle Ctrl+D using '\n'

--- _getline.c
+++ _getline.c
@@ -1,4 +1,18 @@
 #include "shell.h"
+void salida2(char **m, int e, char *line)
+{
+        free(line);
+        free_grid(m, e);
+        write(STDIN_FILENO, "#cisfun$ ", 9);
+        exit(0);
+}
+void salida1(char **m, int e, char *line)
+{
+	free(line);
+	free_grid(m, e);
+	write(STDIN_FILENO, "\n", 1);
+	exit(0);
+}
 /**
  * _getline - function to read what the user writes
  * @a: pointer to loop counter
@@ -10,8 +24,10 @@ char  *_getline(int *a, char **m, int e)
 {
 	char letter[1] = {0}, *line = NULL;
 	size_t bufsize = 0;
-	int num = 1;
+	static int num = 1;
 
+	if (num == 2)
+		salida2(m, e, line);
 	while (num != 0)
 	{
 		bufsize = 0;
@@ -22,30 +38,31 @@ char  *_getline(int *a, char **m, int e)
 		while ((num = read(STDIN_FILENO, letter, 1)) > 0)
 		{
 			if (bufsize == 0)
-				line = _calloc(bufsize + 1, sizeof(char));
+				line = _calloc(bufsize + 2, sizeof(char));
 			else
-				line = _realloc(line, bufsize, bufsize + 1);
+				line = _realloc(line, bufsize, bufsize + 2);
 			if (!line)
 			{
 				num = 0;
 				break;
 			}
 			line[bufsize] = letter[0];
+			line[bufsize + 1] = '\n';
 			if (line[bufsize] == '\n')
 				break;
 			bufsize++;
 		}
-		if (!line && num == 0)
+		if (num == 0 && bufsize == 0)
 			break;
-		if (line[0] != '\n')
+		else if (num == 0 && bufsize != 0)
+		{
+			num = 2;
+			break;
+		}
+		else if (line[0] != '\n')
 			return (line);
 	}
 	if (num == 0)
-	{
-		free(line);
-		free_grid(m, e);
-		write(STDIN_FILENO, "\n", 1);
-		exit(0);
-	}
-	return (NULL);
+		salida1(m, e, line);
+	return (line);
 }

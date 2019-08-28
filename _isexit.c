2b3c60a5c4f3fe36101fd31f5cc38879196372b5
jorgezafra94 <947@holbertonschool.com>
2019-08-28 11:42:02 +0000

    [fix] _isexit

--- _isexit.c
+++ _isexit.c
@@ -23,10 +23,21 @@ void _type(char **p, int L, char *l, int i, char **v, char **m, int e, char *f)
 	}
 	else
 	{
+		while (p[1][c] != '\0')
+		{
+			if (p[1][0] != '+' && p[1][0] != '-' &&
+			    p[1][c] < 48 || p[1][c] > 57)
+			{
+				flag = 1;
+				break;
+			}
+			c++;
+		}
 		if (flag == 1)
 			_put_err(p, L, 1, v);
 		else
-		{ valor = _atoi(p[1]);
+		{
+			valor = _atoi(p[1]);
 			if (!(valor > INT_MAX) && valor > 0)
 			{
 				valor = valor % 256;
